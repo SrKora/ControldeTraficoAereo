7beb1b729003f4458e68540d3d0d94d37440cbee
SrKora <kora22touma@gmail.com>
2025-06-19 17:44:18 +0200

    modificando metodo siguiente evento

--- src/main/java/com/torreDeControl/Main.java
+++ src/main/java/com/torreDeControl/Main.java
@@ -1,6 +1,5 @@
 package com.torreDeControl;
 
-import com.torreDeControl.naves.Aeronave;
 import com.torreDeControl.naves.subnaves.Carga;
 import com.torreDeControl.naves.subnaves.Emergencia;
 import com.torreDeControl.naves.subnaves.Militar;
@@ -19,141 +18,175 @@ public class Main {
         int decision;
         boolean sw = true;
         String id;
-        do {
+        try{
             do {
-                System.out.println("""
+                do {
+                    System.out.println("""
                     Selecciona una opción:
                     1. Registrar nueva aeronave
                     2. Organizar aterrizaje
-                    3. Despejar pista
+                    3. Organizar pista
                     4. Organizar despegue
-                    5. Listar cola de aterrizaje
-                    6. Listar cola de despegue
-                    7. Salir
+                    5. Procesar siguiente evento
+                    6. Listar cola de aterrizaje
+                    7. Listar cola de despegue
+                    8. Salir
                     """);
-                decision = sc.nextInt();
-            } while (decision < 1  || decision > 7);
-            switch (decision) {
-                case 1:
-                    do {
-                        System.out.println("Que tipo de Aeronave quiere registrar?");
-                        System.out.println("""
+                    decision = sc.nextInt();
+                } while (decision < 1  || decision > 8);
+                switch (decision) {
+                    case 1:
+                        do {
+                            System.out.println("Que tipo de Aeronave quiere registrar?");
+                            System.out.println("""
                             1. Aeronave de Emergencias
                             2. Aeronave Militar
                             3. Aeronave para Pasajeros
                             4. Aeronave Carga
                             """);
-                        decision = sc.nextInt();
-                    } while (decision < 1  || decision > 4);
-                    do {
-                        System.out.println("Ingresa el ID de la aeronave:   (Ej: IB3721)");
-                        id = sc.next();
-                    } while (!id.matches("[A-Z]{1,2}[0-9]{1,4}"));
-                    switch (decision) {
-                        case 1:
-                            // Registra nave de Emergencias
-                            Emergencia e = new Emergencia(id, "Emergencias", 1);
-                            t.registrarAeronaveParaAterrizaje(e);
-                            System.out.println("Aeronave de Emergencias registrada");
-                            log.infoLog("Aeronave de Emergencias registrada ID: " + e.getId());
-                            break;
-                        case 2:
-                            // Registra nave militar
-                            Militar m = new Militar(id, "Militar", 2);
-                            t.registrarAeronaveParaAterrizaje(m);
-                            System.out.println("Aeronave Militar registrada");
-                            log.infoLog("Aeronave Militar registrada ID: " + m.getId());
-                            break;
-                        case 3:
-                            // Registra nave de pasajeros
-                            Pasajeros p = new Pasajeros(id, "Pasajeros", 3);
-                            t.registrarAeronaveParaAterrizaje(p);
-                            System.out.println("Aeronave de Pasajeros registrada");
-                            log.infoLog("Aeronave de Pasajeros registrada ID: " + p.getId());
-                            break;
-                        case 4:
-                            // Registra nave de carga
-                            Carga c = new Carga(id, "Carga", 4);
-                            t.registrarAeronaveParaAterrizaje(c);
-                            System.out.println("Aeronave de Carga registrada");
-                            log.infoLog("Aeronave de Carga registrada ID: " + c.getId());
-                            break;
-                    }
-                    break;
-                case 2:
-                    if (!t.getColaDeAterrizaje().isEmpty()) {
-                        if (t.pista == false) {
-                            // La pista se ocupa
-                            t.setAeronaveEnPista(t.getColaDeAterrizaje().getLast());
-                            t.setPista(true);
+                            decision = sc.nextInt();
+                        } while (decision < 1  || decision > 4);
+                        do {
+                            System.out.println("Ingresa el ID de la aeronave:   (Ej: IB3721)");
+                            id = sc.next();
+                        } while (!id.matches("[A-Z]{1,2}[0-9]{1,4}") && t.comprobarID(id));
+                        switch (decision) {
+                            case 1:
+                                // Registra nave de Emergencias
+                                Emergencia e = new Emergencia(id, "Emergencias", 1);
+                                t.registrarAeronaveParaAterrizaje(e);
+                                System.out.println("Aeronave de Emergencias registrada");
+                                log.infoLog("Aeronave de Emergencias registrada ID: " + e.getId());
+                                break;
+                            case 2:
+                                // Registra nave militar
+                                Militar m = new Militar(id, "Militar", 2);
+                                t.registrarAeronaveParaAterrizaje(m);
+                                System.out.println("Aeronave Militar registrada");
+                                log.infoLog("Aeronave Militar registrada ID: " + m.getId());
+                                break;
+                            case 3:
+                                // Registra nave de pasajeros
+                                Pasajeros p = new Pasajeros(id, "Pasajeros", 3);
+                                t.registrarAeronaveParaAterrizaje(p);
+                                System.out.println("Aeronave de Pasajeros registrada");
+                                log.infoLog("Aeronave de Pasajeros registrada ID: " + p.getId());
+                                break;
+                            case 4:
+                                // Registra nave de carga
+                                Carga c = new Carga(id, "Carga", 4);
+                                t.registrarAeronaveParaAterrizaje(c);
+                                System.out.println("Aeronave de Carga registrada");
+                                log.infoLog("Aeronave de Carga registrada ID: " + c.getId());
+                                break;
+                        }
+                        break;
+                    case 2:
+                        if (!t.getColaDeAterrizaje().isEmpty()) {
+                            if (t.pista == false) {
+                                // La pista se ocupa
+                                System.out.println("Aeronave " + t.getColaDeAterrizaje().getLast().getId() + " aterrizando...");
+                                t.setAeronaveEnPista(t.getColaDeAterrizaje().getLast());
+                                t.setPista(true);
+                                t.failError();
 
-                            // Elimina el avión acabado de aterrizar de la cola de aterrizajes
-                            t.getColaDeAterrizaje().removeLast();
+                                // Elimina el avión acabado de aterrizar de la cola de aterrizajes
+                                t.getColaDeAterrizaje().removeLast();
 
-                            // Logs
-                            log.infoLog("Aeronave aterrizando en pista ID: " + t.getAeronaveEnPista().getId() + " Prioridad: " + t.getAeronaveEnPista().getPrioridad());
-                            System.out.println("Aeronave aterrizando en pista ID: " + t.getAeronaveEnPista().getId() + " Prioridad: " + t.getAeronaveEnPista().getPrioridad());
+                                // Introduce el avión en pista a la cola de despegues
+                                t.registrarAeronaveParaDespegue(t.getAeronaveEnPista());
 
-                            t.setPista(true);
+                                // Logs
+                                log.infoLog("Aeronave aterrizada, ID: " + t.getAeronaveEnPista().getId() + " Prioridad: " + t.getAeronaveEnPista().getPrioridad());
+                                System.out.println("Aeronave aterrizada, ID: " + t.getAeronaveEnPista().getId() + " Prioridad: " + t.getAeronaveEnPista().getPrioridad());
+
+                                t.setPista(true);
+                            } else {
+                                // Logs
+                                log.errorLog("Pista ocupada, aterrizaje no permitido");
+                                System.err.println("Pista ocupada, aterrizaje no permitido");
+                            }
                         } else {
                             // Logs
-                            log.errorLog("Pista ocupada, aterrizaje no permitido");
-                            System.err.println("Pista ocupada, aterrizaje no permitido");
+                            log.errorLog("Ningúna aeronave registrada para aterrizaje");
+                            System.err.println("Ningúna aeronave registrada para aterrizaje");
                         }
-                    } else {
-                        // Logs
-                        log.errorLog("Ningúna aeronave registrada para aterrizaje");
-                        System.err.println("Ningúna aeronave registrada para aterrizaje");
-                    }
-                    break;
-                case 3:
-                    if (t.pista == true) {
-                        t.liberarPista();
-                        // Introduce el avión en pista a la cola de despegues
-                        t.registrarAeronaveParaDespegue(t.getAeronaveEnPista());
+                        break;
+                    case 3:
+                        if (t.pista == true) {
+                            t.liberarPista();
 
-                        // Logs
-                        log.infoLog("Aeronave ocupando pista ID: " + t.getAeronaveEnPista().getId() + " Prioridad: " + t.getAeronaveEnPista().getPrioridad());
-                        System.out.println("Aeronave ocupando pista ID: " + t.getAeronaveEnPista().getId() + " Prioridad: " + t.getAeronaveEnPista().getPrioridad());
-
-                        t.setAeronaveEnPista(null);
-                    } else {
-                        // Logs
-                        log.errorLog("Pista despejada, ninguna aeronave en pista");
-                        System.err.println("Pista despejada, ninguna aeronave en pista");
-                    }
-                    break;
-                case 4:
-                    if (!t.getColaDeDespegues().isEmpty()) {
-                        if (t.pista == false) {
-
-                            t.setAeronaveEnPista(t.getColaDeDespegues().getLast());
-                            t.getColaDeDespegues().removeLast();
                             // Logs
-                            log.infoLog("Aeronave despegando ID: " + t.getAeronaveEnPista().getId()  + " Prioridad: " + t.getAeronaveEnPista().getPrioridad() + ", pista despejada");
-                            System.out.println("Aeronave despegando ID: " + t.getAeronaveEnPista().getId()  + " Prioridad: " + t.getAeronaveEnPista().getPrioridad() + ", pista despejada");
+                            log.infoLog("Aeronave ocupando pista ID: " + t.getAeronaveEnPista().getId() + " Prioridad: " + t.getAeronaveEnPista().getPrioridad());
+                            System.out.println("Aeronave ocupando pista ID: " + t.getAeronaveEnPista().getId() + " Prioridad: " + t.getAeronaveEnPista().getPrioridad());
+
+                            System.out.println("Desocupando pista...");
+
+                            t.failError();
+
                             t.setAeronaveEnPista(null);
                         } else {
                             // Logs
-                            log.errorLog("Pista vacía, no hay aeronaves que puedan despegar");
-                            System.err.println("Pista vacía, no hay aeronaves que puedan despegar");
+                            log.errorLog("Pista despejada, ninguna aeronave en pista");
+                            System.err.println("Pista despejada, ninguna aeronave en pista");
                         }
-                    } else {
-                        log.errorLog("Ningúna aeronave registrada para despegue");
-                        System.err.println("Ningúna aeronave registrada para despegue");
-                    }
+                        break;
+                    case 4:
+                        if (!t.getColaDeDespegues().isEmpty()) {
+                            // Si la pista está vacía saca una aeronave y la despega
+                            if (t.pista == false) {
+                                System.out.println("Avión " + t.getColaDeDespegues().getLast().getId() + " ocupando pista");
+                                t.setAeronaveEnPista(t.getColaDeDespegues().getLast());
 
-                    break;
-                case 5:
-                    t.listarNaves(t.getColaDeAterrizaje());
-                    break;
-                case 6:
-                    t.listarNaves(t.getColaDeDespegues());
-                    break;
-                default:
-                    sw = false;
-                    break;
-            }
-        } while (sw);
+                                t.failError();
+
+                                t.getColaDeDespegues().removeLast();
+
+                                // Logs
+                                log.infoLog("Aeronave despegando ID: " + t.getAeronaveEnPista().getId()  + " Prioridad: " + t.getAeronaveEnPista().getPrioridad() + ", pista despejada");
+                                System.out.println("Aeronave despegando ID: " + t.getAeronaveEnPista().getId()  + " Prioridad: " + t.getAeronaveEnPista().getPrioridad() + ", pista despejada");
+                                t.setAeronaveEnPista(null);
+                            // Si no, la pista estará ocupada y despegará el avión que la estaba ocupando
+                            } else if (t.pista) {
+                                System.out.println("Avión " + t.aeronaveEnPista.getId() + " ocupando pista");
+
+                                System.out.println("Avión " + t.aeronaveEnPista.getId() + " despegando...");
+
+                                t.failError();
+
+                                t.getColaDeDespegues().removeLast();
+
+                                // logs
+                                log.infoLog("Aeronave despegando ID: " + t.getAeronaveEnPista().getId()  + " Prioridad: " + t.getAeronaveEnPista().getPrioridad() + ", pista despejada");
+                                System.out.println("Aeronave despegando ID: " + t.getAeronaveEnPista().getId()  + " Prioridad: " + t.getAeronaveEnPista().getPrioridad() + ", pista despejada");
+
+                                t.setAeronaveEnPista(null);
+
+                                t.liberarPista();
+                            }
+                        } else {
+                            // logs
+                            log.errorLog("Ningúna aeronave registrada para despegue");
+                            System.err.println("Ningúna aeronave registrada para despegue");
+                        }
+                        break;
+                    case 5:
+                        t.procesarSiguienteEvento(t);
+                        break;
+                    case 6:
+                        t.listarNaves(t.getColaDeAterrizaje());
+                        break;
+                    case 7:
+                        t.listarNaves(t.getColaDeDespegues());
+                        break;
+                    default:
+                        sw = false;
+                        break;
+                }
+            } while (sw);
+        } catch (Exception e) {
+            log.errorLog(e.getMessage());
+        }
+
     }
 }
--- src/main/java/com/torreDeControl/TorreDeControl.java
+++ src/main/java/com/torreDeControl/TorreDeControl.java
@@ -1,6 +1,11 @@
 package com.torreDeControl;
 
 import com.torreDeControl.naves.Aeronave;
+import com.torreDeControl.naves.subnaves.Carga;
+import com.torreDeControl.naves.subnaves.Emergencia;
+import com.torreDeControl.naves.subnaves.Militar;
+import com.torreDeControl.naves.subnaves.Pasajeros;
+
 import java.util.*;
 
 public class TorreDeControl {
@@ -8,6 +13,9 @@ public class TorreDeControl {
     ArrayList<Aeronave> colaDeDespegues = new ArrayList<>();
     Boolean pista = false;
     Aeronave aeronaveEnPista;
+    Scanner sc = new Scanner(System.in);
+
+    ErrorSystem log = new ErrorSystem();
 
     public void registrarAeronaveParaAterrizaje(Aeronave a) {
         colaDeAterrizaje.add(a);
@@ -29,7 +37,197 @@ public class TorreDeControl {
         });
     }
 
-    public void procesarSiguienteEvento() {
+    public void procesarSiguienteEvento(TorreDeControl t) {
+
+    }
+
+    private void menuInteraccion(TorreDeControl t, int n) {
+        switch (n) {
+            case 1:
+                aterrizarAeronave(t);
+                break;
+            case 2:
+                liberarPista(t);
+                break;
+            case 3:
+                despegarAeronave(t);
+                break;
+        }
+    }
+
+    private void despegarAeronave(TorreDeControl t) {
+        if (!t.getColaDeDespegues().isEmpty()) {
+            // Si la pista está vacía saca una aeronave y la despega
+            System.out.println("Avión " + t.aeronaveEnPista.getId() + " ocupando pista");
+            if (t.pista == false) {
+                t.setAeronaveEnPista(t.getColaDeDespegues().getLast());
+
+                t.failError();
+
+                t.getColaDeDespegues().removeLast();
+
+                // Logs
+                log.infoLog("Aeronave despegando ID: " + t.getAeronaveEnPista().getId()  + " Prioridad: " + t.getAeronaveEnPista().getPrioridad() + ", pista despejada");
+                System.out.println("Aeronave despegando ID: " + t.getAeronaveEnPista().getId()  + " Prioridad: " + t.getAeronaveEnPista().getPrioridad() + ", pista despejada");
+                t.setAeronaveEnPista(null);
+                // Si no, la pista estará ocupada y despegará el avión que la estaba ocupando
+            } else {
+
+                System.out.println("Avión " + t.aeronaveEnPista.getId() + " despegando...");
+
+                t.failError();
+
+                t.getColaDeDespegues().removeLast();
+
+                // logs
+                log.infoLog("Aeronave despegando ID: " + t.getAeronaveEnPista().getId()  + " Prioridad: " + t.getAeronaveEnPista().getPrioridad() + ", pista despejada");
+                System.out.println("Aeronave despegando ID: " + t.getAeronaveEnPista().getId()  + " Prioridad: " + t.getAeronaveEnPista().getPrioridad() + ", pista despejada");
+
+                t.setAeronaveEnPista(null);
+
+                t.liberarPista();
+            }
+        } else {
+            // logs
+            log.errorLog("Ningúna aeronave registrada para despegue");
+            System.err.println("Ningúna aeronave registrada para despegue");
+        }
+    }
+
+    private void liberarPista(TorreDeControl t) {
+        if (t.pista == true) {
+            t.liberarPista();
+
+            // Logs
+            log.infoLog("Aeronave ocupando pista ID: " + t.getAeronaveEnPista().getId() + " Prioridad: " + t.getAeronaveEnPista().getPrioridad());
+            System.out.println("Aeronave ocupando pista ID: " + t.getAeronaveEnPista().getId() + " Prioridad: " + t.getAeronaveEnPista().getPrioridad());
+
+            System.out.println("Desocupando pista...");
+
+            t.failError();
+
+            t.setAeronaveEnPista(null);
+        } else {
+            // Logs
+            log.errorLog("Pista despejada, ninguna aeronave en pista");
+            System.err.println("Pista despejada, ninguna aeronave en pista");
+        }
+    }
+
+    private void aterrizarAeronave(TorreDeControl t) {
+        if (!t.getColaDeAterrizaje().isEmpty()) {
+            if (t.pista == false) {
+                // La pista se ocupa
+                System.out.println("Aeronave " + t.getColaDeAterrizaje().getLast().getId() + " aterrizando...");
+                t.setAeronaveEnPista(t.getColaDeAterrizaje().getLast());
+                t.setPista(true);
+                t.failError();
+
+                // Elimina el avión acabado de aterrizar de la cola de aterrizajes
+                t.getColaDeAterrizaje().removeLast();
+
+                // Introduce el avión en pista a la cola de despegues
+                t.registrarAeronaveParaDespegue(t.getAeronaveEnPista());
+
+                // Logs
+                log.infoLog("Aeronave aterrizada, ID: " + t.getAeronaveEnPista().getId() + " Prioridad: " + t.getAeronaveEnPista().getPrioridad());
+                System.out.println("Aeronave aterrizada, ID: " + t.getAeronaveEnPista().getId() + " Prioridad: " + t.getAeronaveEnPista().getPrioridad());
+
+                t.setPista(true);
+            } else {
+                // Logs
+                log.errorLog("Pista ocupada, aterrizaje no permitido");
+                System.err.println("Pista ocupada, aterrizaje no permitido");
+            }
+        } else {
+            // Logs
+            log.errorLog("Ningúna aeronave registrada para aterrizaje");
+            System.err.println("Ningúna aeronave registrada para aterrizaje");
+        }
+    }
+
+    public void crearAeronave (TorreDeControl t, int decision) {
+        String id;
+        do {
+            id = randomID();
+        } while (!id.matches("[A-Z]{1,2}[0-9]{1,4}") && t.comprobarID(id));
+        switch (decision) {
+            case 1:
+                // Registra nave de Emergencias
+                Emergencia e = new Emergencia(id, "Emergencias", 1);
+                t.registrarAeronaveParaAterrizaje(e);
+                System.out.println("Aeronave de Emergencias registrada ID: " + e.getId());
+                log.infoLog("Aeronave de Emergencias registrada ID: " + e.getId());
+                break;
+            case 2:
+                // Registra nave militar
+                Militar m = new Militar(id, "Militar", 2);
+                t.registrarAeronaveParaAterrizaje(m);
+                System.out.println("Aeronave Militar registrada ID: " + m.getId());
+                log.infoLog("Aeronave Militar registrada ID: " + m.getId());
+                break;
+            case 3:
+                // Registra nave de pasajeros
+                Pasajeros p = new Pasajeros(id, "Pasajeros", 3);
+                t.registrarAeronaveParaAterrizaje(p);
+                System.out.println("Aeronave de Pasajeros registrada ID: " + p.getId());
+                log.infoLog("Aeronave de Pasajeros registrada ID: " + p.getId());
+                break;
+            case 4:
+                // Registra nave de carga
+                Carga c = new Carga(id, "Carga", 4);
+                t.registrarAeronaveParaAterrizaje(c);
+                System.out.println("Aeronave de Carga registrada ID: " + c.getId());
+                log.infoLog("Aeronave de Carga registrada ID: " + c.getId());
+                break;
+        }
+    }
+
+    public String randomID() {
+        String ID = String.valueOf(letraAleatoria());
+        ID += String.valueOf(letraAleatoria());
+        ID  += String.valueOf(1000 + random(9000));
+        return ID;
+    }
+
+    public char letraAleatoria() {
+        return (char) ('A' + (int)(Math.random() * 26));
+    }
+
+    public void failError(){
+        if (random(100) <= 12){
+            try {
+                int ms = random(10000) + 3000;
+                System.out.println("Fallo en el sistema tiempos de esperas cambiados, tiempo de espera " + ms/1000 + " seg");
+                Thread.sleep(ms);
+            } catch (InterruptedException e) {
+                log.errorLog(e.getMessage());
+            }
+        } else {
+            try {
+                Thread.sleep(3000 + random(1500));
+            } catch (InterruptedException e) {
+                log.errorLog(e.getMessage());
+            }
+        }
+    }
+
+    public boolean comprobarID (String id){
+        for (Aeronave a : colaDeAterrizaje){
+            if (a.getId().equals(id)){
+                return true;
+            }
+        }
+        for (Aeronave a : colaDeDespegues){
+            if (a.getId().equals(id)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int random(int n) {
+        return (int) (Math.random() * n);
     }
 
     public void liberarPista() {
